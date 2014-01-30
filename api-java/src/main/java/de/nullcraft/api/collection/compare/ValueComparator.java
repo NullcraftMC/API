05527e52f304f8a0da0ba84100777c4ff1016b01
maxikg <maxmax58@googlemail.com>
2014-01-30 14:56:59 +0100

    Simpler generics

--- api-java/src/main/java/de/nullcraft/api/collection/compare/ValueComparator.java
+++ api-java/src/main/java/de/nullcraft/api/collection/compare/ValueComparator.java
@@ -6,17 +6,17 @@ import java.util.Map;
 /**
  * @author maxikg <me@maxikg.de>
  */
-public class ValueComparator<K, V> implements Comparator<K> {
+public class ValueComparator<V> implements Comparator<Object> {
 
-    private final Map<K, V> base;
+    private final Map<? extends Object, V> base;
     private final Comparator<V> comparator;
 
-    public ValueComparator(Map<K, V> base, Comparator<V> comparator) {
+    public ValueComparator(Map<? extends Object, V> base, Comparator<V> comparator) {
         this.base = base;
         this.comparator = comparator;
     }
 
-    public int compare(K a, K b) {
+    public int compare(Object a, Object b) {
         synchronized (base) {
             return comparator.compare(base.get(a), base.get(b));
         }
